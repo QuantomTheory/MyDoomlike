073a75411597f8af815bd5321f53ffc0ce3818e1
QuantomTheory <106330956+QuantomTheory@users.noreply.github.com>
2022-10-25 13:04:44 +0100

    Delete MyDoomlike directory

--- MyDoomlike/MyDoomlike.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "MyDoomlike.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyDoomlike, "MyDoomlike" );
- 
